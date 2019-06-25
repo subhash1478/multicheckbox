5f8589955b7c4adfe7724bd5843ccb502ba29cbd
svrock <putumca@gmail.com>
2019-06-25 22:14:52 +0530

    multiple checkbox

---
+++ checkbox.js
@@ -0,0 +1,30 @@
+$(function () {
+    disableCheckOut();
+    $('.chckHead').click(function () {
+        if ($(".chckHead").prop('checked')) {
+            $('.chcktbl').prop('checked', true);
+            disableCheckOut();
+        } else {
+            $('.chcktbl').prop('checked', false)
+            disableCheckOut();
+        }
+    });
+    $('.chcktbl').click(function () {
+        disableCheckOut();
+    });
+    $('#btnSubmit').click(function () {
+        var value = [];
+        $('.chcktbl:checked').each(function (res) {
+            value.push($(this).val())
+        })
+        console.log(value);
+        alert(value)
+    });
+});
+function disableCheckOut() {
+    if ($('.chcktbl:checked').length == 0) {
+        $("#btnSubmit").attr("disabled", true);
+    } else {
+        $("#btnSubmit").attr("disabled", false);
+    }
+}
